6a0a3d2227c1091db54a477c7dd092cfbd2e7a1b
Swastik Makwana <mswastik2004@gmail.com>
2023-11-07 11:29:21 +0530

    Ver 1

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mad_practical_11_21012011046"
+include(":app")
+ 
